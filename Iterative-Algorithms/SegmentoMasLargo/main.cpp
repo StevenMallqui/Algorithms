1e06bbef502667df6485dfec47be504328d79b3f
StevenMallqui <57865326+StevenMallqui@users.noreply.github.com>
2024-06-03 19:34:29 +0200

    fal

--- Iterative-Algorithms/SegmentoMasLargo/main.cpp
+++ Iterative-Algorithms/SegmentoMasLargo/main.cpp
@@ -10,21 +10,49 @@ using namespace std;
 
 
 // función que resuelve el problema
-TipoSolucion resolver(TipoDatos datos) {
-    
-    
+int resolver(vector<int> const& datos) {
+    int longitud = 1, cont = 1, auxLongitud = 1;
+
+    if (datos.size() <= 2)
+        longitud = datos.size();
+    else {
+        for (long unsigned int i = 1; i < datos.size(); i++) {
+            if (datos[i - 1] < datos[i])
+                cont++;
+            else
+                cont = 1;
+
+            auxLongitud++;
+
+            if (cont == 3) {
+                longitud = max(auxLongitud - 1, longitud);
+                auxLongitud = 2;
+                cont = 2;
+            } else {
+                longitud = max(auxLongitud, longitud);
+            }
+        }
+    }
+  
+    return longitud;
 }
 
 // Resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
 void resuelveCaso() {
     // leer los datos de la entrada
+    int N, aux; cin >> N;
+    vector<int> datos;
+
+    for(int i = 0; i < N; i++){
+        cin >> aux;
+        datos.push_back(aux);
+    }
     
-    
-    TipoSolucion sol = resolver(datos);
+    int sol = resolver(datos);
     // escribir sol
     
-    
+    cout << sol <<'\n';
 }
 
 int main() {
